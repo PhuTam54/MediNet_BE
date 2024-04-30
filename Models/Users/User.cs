42eea9c6c5a1969e0ac94e7687723a25953e0995
NguyenHuuQuanT2210A <118426160+NguyenHuuQuanT2210A@users.noreply.github.com>
2024-04-30 17:36:28 +0700

    update code part 2
    update code part 2

--- Models/Users/User.cs
+++ Models/Users/User.cs
@@ -4,7 +4,7 @@
     {
 		public int Id { get; set; }
 		public string Username { get; set; }
-		public string Slug { get; set; }
+		public string SEO_Name { get; set; }
 		public string Email { get; set; }
 		public string Password { get; set; }
 		public int Role { get; set; }
